401dbaa46726840ecd3b2f67a0e0c98d9db03aab
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-04-20 13:49:12 -0500

    search functionality now working

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const PORT = process.env.PORT || 3000;
 const request = require('request');
 app.set('view engine', 'ejs');
 
+app.get('/', function(req, res) {
+  res.render('search');
+});
+
 app.get('/results', function(req, res) {
-  request('http://www.omdbapi.com/?s=texas&apikey=ba7f203a', function(
-    error,
-    response,
-    body
-  ) {
+  var query = req.query.search;
+  var url = 'http://www.omdbapi.com/?s=' + query + '&apikey=ba7f203a';
+  request(url, function(error, response, body) {
     if (!error && response.statusCode == 200) {
       var data = JSON.parse(body);
       console.log(data);
