33b42db231584ae49960ca235473a40842c93362
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-04-20 10:49:14 -0500

    implemented ejs

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 const request = require('request');
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-app.get("/results", function(req, res) {
-    request("http://www.omdbapi.com/?s=texas&apikey=ba7f203a", function(error, res, body){
-        if(!error && res.statusCode == 200) {
-            var results = JSON.parse(body)
-            res.send(results);
-        }
-    });
+app.get('/results', function(req, res) {
+  request('http://www.omdbapi.com/?s=texas&apikey=ba7f203a', function(
+    error,
+    res,
+    body
+  ) {
+    if (!error && res.statusCode == 200) {
+      var data = JSON.parse(body);
+      console.log(data);
+      res.render('results', { data: data });
+    }
+  });
 });
 
-
 app.listen(3000, () => {
-    console.log(`Movie App has started on port ${PORT}`);
-}); 
+  console.log(`Movie App has started on port ${PORT}`);
+});
